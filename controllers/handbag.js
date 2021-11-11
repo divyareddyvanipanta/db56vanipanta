252507e8f77425e041f2cf79bd7b3e97605d24d3
divyareddyvanipanta <divyareddyvanipanta@gmail.com>
2021-11-10 21:08:06 -0600

    added part 7

--- controllers/handbag.js
+++ controllers/handbag.js
@@ -16,8 +16,25 @@ exports.handbag_detail = function(req, res) {
  res.send('NOT IMPLEMENTED: Handbag detail: ' + req.params.id);
 };
 // Handle handbag create on POST.
-exports.handbag_create_post = function(req, res) {
- res.send('NOT IMPLEMENTED: handbag create POST');
+exports.handbag_create_post = async function(req, res) {
+    console.log(req.body)
+    let document = new Handbag();
+    // We are looking for a body, since POST does not have query parameters.
+    // Even though bodies can be in many different formats, we will be picky
+    // and require that it be a json object
+    // {"costume_type":"goat", "cost":12, "size":"large"}
+    document.bag_name = req.body.bag_name;
+    document.bag_color = req.body.bag_color;
+    document.bag_cost = req.body.bag_cost;
+    try{
+    let result = await document.save();
+    res.send(result);
+    }
+    catch(err){
+    res.status(500);
+    res.send(`{"error": ${err}}`);
+    }
+// res.send('NOT IMPLEMENTED: handbag create POST');
 };
 // Handle handbag delete form on DELETE.
 exports.handbag_delete = function(req, res) {
@@ -27,3 +44,16 @@ exports.handbag_delete = function(req, res) {
 exports.handbag_update_put = function(req, res) {
  res.send('NOT IMPLEMENTED: handbag update PUT' + req.params.id);
 };
+
+// VIEWS
+// Handle a show all view
+exports.handbag_view_all_Page = async function(req, res) {
+    try{
+    theHandbags = await Handbag.find();
+    res.render('handbags', { title: 'Handbags Search Results', results: theHandbags });
+    }
+    catch(err){
+    res.status(500);
+    res.send(`{"error": ${err}}`);
+    }
+   };
--- routes/handbag.js
+++ routes/handbag.js
@@ -1,4 +1,5 @@
 var express = require('express');
+const handbag_controlers= require('../controllers/handbag');
 var router = express.Router();
 
 /* GET home page. */
@@ -6,4 +7,6 @@ router.get('/', function(req, res, next) {
   res.render('handbag', { title: 'Search Results Hand Bags' });
 });
 
+/* GET costumes */
+router.get('/', handbag_controlers.handbag_view_all_Page );
 module.exports = router;
