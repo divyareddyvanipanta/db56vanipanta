51cfe02b3272995e0714803e3cf2f1fc745b9de3
divyareddyvanipanta <divyareddyvanipanta@gmail.com>
2021-11-30 22:01:07 -0600

    added part 5

--- models/handbag.js
+++ models/handbag.js
@@ -2,6 +2,7 @@ const mongoose = require("mongoose")
 const handbagSchema = mongoose.Schema({
 bag_name: String,
 bag_color: String,
-bag_cost: Number
+bag_cost: Number,
+bag_size: Number,
 })
 module.exports = mongoose.model("Handbag",handbagSchema)
