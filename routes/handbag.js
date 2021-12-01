a072f09d44050e35c8e20d1915169b44aeef33a6
divyareddyvanipanta <divyareddyvanipanta@gmail.com>
2021-11-30 21:36:46 -0600

    part 3 and 4 added

--- routes/handbag.js
+++ routes/handbag.js
@@ -2,6 +2,17 @@ var express = require('express');
 const handbag_controlers= require('../controllers/handbag');
 var router = express.Router();
 
+// A little function to check if we have an authorized user and continue on
+// or
+// redirect to login.
+const secured = (req, res, next) => {
+    if (req.user){
+    return next();
+    }
+    req.session.returnTo = req.originalUrl;
+    res.redirect("/login");
+}
+
 /* GET home page. */
 // router.get('/', function(req, res, next) {
 //   res.render('handbag', { title: 'Search Results Hand Bags' });
@@ -20,7 +31,7 @@ router.get('/detail', handbag_controlers.handbag_view_one_Page);
 router.get('/create', handbag_controlers.handbag_create_Page);
 
 /* GET create update page */
-router.get('/update', handbag_controlers.handbag_update_Page);
+router.get('/update', secured, handbag_controlers.handbag_update_Page);
 
 /* GET create costume page */
 router.get('/delete', handbag_controlers.handbag_delete_Page);
